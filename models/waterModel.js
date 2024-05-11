575b05c2201ee27d57e9b97d1655b7b4edd3ca95
Yevgenii <126769681+uncle-Jonik@users.noreply.github.com>
2024-05-11 12:34:41 +0300

    Update waterModel.js

--- models/waterModel.js
+++ models/waterModel.js
@@ -3,11 +3,15 @@ import { model, Schema } from "mongoose";
 const waterSchema = Schema(
   {
     //
-    owner: {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
+    // owner: {
+    //   type: Schema.Types.ObjectId,
+    //   ref: "User",
+    //   required: true,
+    // },
+    date: { type: Date },
+    waterRate: { type: Number, default: 0 },
+    process: { type: Number, default: 0 },
+    complete: { type: Boolean, default: false },
   },
   {
     timestamps: true,
