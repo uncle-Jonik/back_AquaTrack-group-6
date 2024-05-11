d5d9a7310db4c9a7d0344e3adcaf25a9ae041f4a
Yevgenii <126769681+uncle-Jonik@users.noreply.github.com>
2024-05-11 16:43:41 +0300

    Update waterValueValidator.js

--- schemas/waterValueValidator.js
+++ schemas/waterValueValidator.js
@@ -8,7 +8,7 @@ export const checkWaterValidator = joiValidator((data) =>
   Joi.object()
     .options({ abortEarly: false })
     .keys({
-      localDate: Joi.string().pattern(new RegExp(DATE_REGEX)),
+      localDate: Joi.string().pattern(new RegExp(DATE_REGEX)).length(10),
       localTime: Joi.string().pattern(new RegExp(TIME_REGEX)).length(5),
       waterValue: Joi.number().required(),
     })
